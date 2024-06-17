bb70c6523fe486955d011e84284c5e639ff03677
vicziani <viczian.istvan@gmail.com>
2024-06-17 13:09:33 +0200

    Scheduled

--- empapp-schedule/src/main/java/empapp/HelloController.java
+++ empapp-schedule/src/main/java/empapp/HelloController.java
@@ -2,8 +2,11 @@ package empapp;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +18,10 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void hello() {
-        log.info("hello controller");
+        log.info(helloService.getClass().getName());
+        log.info("hello controller 3");
         helloService.hello();
     }
 }
--- empapp-schedule/src/main/java/empapp/HelloService.java
+++ empapp-schedule/src/main/java/empapp/HelloService.java
@@ -3,6 +3,7 @@ package empapp;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +15,10 @@ public class HelloService {
     public void hello() {
         Thread.sleep(5000);
         log.info("hello");
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void scheduled() {
+        log.info("tick");
     }
 }
