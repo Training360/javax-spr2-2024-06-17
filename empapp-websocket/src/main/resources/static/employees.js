1ea9d6026e632a4e9e845b3870a5b7ee2a741e0f
vicziani <viczian.istvan@gmail.com>
2024-06-18 11:39:07 +0200

    Subscribe

--- empapp-websocket/src/main/resources/static/employees.js
+++ empapp-websocket/src/main/resources/static/employees.js
@@ -2,6 +2,18 @@ window.onload = function () {
     const socket = new SockJS("/websocket-endpoint");
     const client = Stomp.over(socket);
 
+    client.connect({}, function (frame) {
+        console.log("Connected")
+        client.subscribe('/topic/employees', function (message) {
+            console.log("Message has come: ", message);
+            const text = JSON.parse(message.body).text;
+            console.log(text);
+
+            const div = document.querySelector("#message-div");
+            div.innerHTML += `<p>${text}</p>`;
+        });
+    });
+
     const button = document.querySelector("#message-button");
     button.onclick = function() {
         const messageInput = document.querySelector("#message-input");
