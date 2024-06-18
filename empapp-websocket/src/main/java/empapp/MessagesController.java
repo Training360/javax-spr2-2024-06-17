f3a1be0c69a5a164c5ba457cf680d791fc8dc6d0
vicziani <viczian.istvan@gmail.com>
2024-06-18 12:02:03 +0200

    Küldés a topicra

--- empapp-websocket/src/main/java/empapp/MessagesController.java
+++ empapp-websocket/src/main/java/empapp/MessagesController.java
@@ -1,14 +1,19 @@
 package empapp;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @Slf4j
+@AllArgsConstructor
 public class MessagesController {
+
+    private SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/messages")
     @SendTo("/topic/employees")
@@ -20,5 +25,7 @@ public class MessagesController {
     @EventListener
     public void handleMessage(Message message) {
         log.info("Spring event has come: {}", message);
+
+        messagingTemplate.convertAndSend("/topic/employees", message);
     }
 }
