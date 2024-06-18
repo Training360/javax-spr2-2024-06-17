98cf3a6144fc06d261ddddf0082cbffca120e589
vicziani <viczian.istvan@gmail.com>
2024-06-18 13:10:50 +0200

    Connected, verziószám változások

--- empapp-websocket-client/src/main/java/employees/EmpappWebsocketClientApplication.java
+++ empapp-websocket-client/src/main/java/employees/EmpappWebsocketClientApplication.java
@@ -4,6 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 @Slf4j
@@ -16,5 +30,14 @@ public class EmpappWebsocketClientApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Hello Command Line");
+
+		var sockJsClient = new SockJsClient(List.of(new WebSocketTransport(new StandardWebSocketClient())));
+		var stompClient = new WebSocketStompClient(sockJsClient);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+		stompClient.connect("ws://localhost:8080/websocket-endpoint",
+				new MyStompMessageHandler());
+
+		new Scanner(System.in).nextLine();
 	}
 }
---
+++ empapp-websocket-client/src/main/java/employees/MyStompMessageHandler.java
@@ -0,0 +1,39 @@
+package employees;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+
+import java.lang.reflect.Type;
+
+@Slf4j
+public class MyStompMessageHandler implements StompSessionHandler {
+
+    @Override
+    public void afterConnected(StompSession stompSession, StompHeaders stompHeaders) {
+        log.info("Connected");
+    }
+
+    @Override
+    public void handleException(StompSession stompSession, StompCommand stompCommand, StompHeaders stompHeaders, byte[] bytes, Throwable throwable) {
+
+    }
+
+    @Override
+    public void handleTransportError(StompSession stompSession, Throwable throwable) {
+
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders stompHeaders) {
+        return null;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders stompHeaders, Object o) {
+
+    }
+}
