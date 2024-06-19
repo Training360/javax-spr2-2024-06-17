e3d84d62f409b282b23dff7fe3bbd89fc3c31038
vicziani <viczian.istvan@gmail.com>
2024-06-19 16:13:55 +0200

    Reaktív alkalmazás init

---
+++ empapp-reactive/src/main/java/training/empappreactive/EmpappReactiveApplication.java
@@ -0,0 +1,13 @@
+package training.empappreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpappReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpappReactiveApplication.class, args);
+    }
+
+}
---
+++ empapp-reactive/src/test/java/training/empappreactive/EmpappReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package training.empappreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpappReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
