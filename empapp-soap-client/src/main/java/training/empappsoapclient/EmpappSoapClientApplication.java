5e428417cd0e4625a8bc9a3a5c14471fb8b4ae5e
vicziani <viczian.istvan@gmail.com>
2024-06-19 11:39:31 +0200

    Alap SOAP kliens

---
+++ empapp-soap-client/src/main/java/training/empappsoapclient/EmpappSoapClientApplication.java
@@ -0,0 +1,13 @@
+package training.empappsoapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpappSoapClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpappSoapClientApplication.class, args);
+	}
+
+}
---
+++ empapp-soap-client/src/test/java/training/empappsoapclient/EmpappSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.empappsoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpappSoapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
