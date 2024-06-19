8c2597c5339c7c6e422890a1bd709111b56a78cf
vicziani <viczian.istvan@gmail.com>
2024-06-19 11:58:59 +0200

    web application type, webszolgáltatás meghívása

--- empapp-soap-client/src/main/java/training/empappsoapclient/EmpappSoapClientApplication.java
+++ empapp-soap-client/src/main/java/training/empappsoapclient/EmpappSoapClientApplication.java
@@ -1,5 +1,6 @@
 package training.empappsoapclient;
 
+import com.training360.services.empapp.EmployeeWebService_Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,5 +17,13 @@ public class EmpappSoapClientApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Hello SOAP");
+
+		var service = new EmployeeWebService_Service();
+		var port = service.getEmployeeWebServicePort();
+		var response = port.listEmployees();
+		var employees = response.getEmployees();
+		for (var employee: employees.getEmployee()) {
+			log.info("Employee: " + employee.getName());
+		}
 	}
 }
