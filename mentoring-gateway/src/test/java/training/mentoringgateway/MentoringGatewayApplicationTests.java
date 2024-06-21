c57a11530efaaf13596916e90370f365269e67dc
vicziani <viczian.istvan@gmail.com>
2024-06-21 11:55:58 +0200

    gateway init

---
+++ mentoring-gateway/src/test/java/training/mentoringgateway/MentoringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package training.mentoringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
